2b6d4c51bf4195592ca6f78220dcfd725cffffb2
Diogo Botas <gamebreak24@gmail.com>
2023-12-28 23:31:35 +0000

    feat: use channels
    Fire each request in 5 seconds intervals.

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -14,19 +15,29 @@ func main() {
 		"https://golang.org",
 	}
 
+	c := make(chan string)
+
 	for _, url := range urls {
-		go checkUrl(url)
+		go checkUrl(url, c)
+	}
+
+	for u := range c {
+		go func(url string) {
+			time.Sleep(5 * time.Second)
+			checkUrl(url, c)
+		}(u)
 	}
 }
 
-func checkUrl(url string) {
+func checkUrl(url string, c chan string) {
 	resp, err := http.Get(url)
 
 	if err != nil {
 		fmt.Println(url, "might be down!")
-		fmt.Println(url, resp.Status)
+		c <- url
 		return
 	}
 
 	fmt.Println(url, "is up! Status:", resp.Status)
+	c <- url
 }
